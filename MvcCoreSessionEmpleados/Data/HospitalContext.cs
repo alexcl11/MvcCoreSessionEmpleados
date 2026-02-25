fcf092ee5003df01bb749905bd15eb28a28645b6
alexcl11 <aleecanovaslopez@gmail.com>
2026-02-25 10:00:39 +0100

    Add project files.

---
+++ MvcCoreSessionEmpleados/Data/HospitalContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreSessionEmpleados.Models;
+
+namespace MvcCoreSessionEmpleados.Data
+{
+    public class HospitalContext: DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext>options) :base(options){ }
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
---
+++ MvcCoreSessionEmpleados/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreSessionEmpleados.Data;
+using MvcCoreSessionEmpleados.Models;
+
+namespace MvcCoreSessionEmpleados.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private HospitalContext context;
+        public RepositoryEmpleados(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            List<Empleado> empleados = await (from datos in this.context.Empleados
+                             select datos).ToListAsync();
+            return empleados;
+        }
+        public async Task<Empleado> FindEmpleadoAsync(int idEmpleado)
+        {
+            Empleado empleado = await (from datos in this.context.Empleados
+                                 where datos.IdEmpleado == idEmpleado
+                                 select datos).FirstOrDefaultAsync();
+            return empleado;
+        }
+    }
+}
